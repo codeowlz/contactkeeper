be8383623d53fc05e6d1d2e90356967bd33b5380
Marcell Tóth <cctmarcell@gmail.com>
2021-03-15 21:59:25 +0100

    MongoDB setup

--- routes/auth.js
+++ routes/auth.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
-// @route   GET api/auth
-//@desc     Get logged in user
-//@access   Private
+// @route     GET api/auth
+// @desc      Get logged in user
+// @access    Private
 router.get("/", (req, res) => {
-  res.send("Get logged in a user");
+  res.send("Get logged in user");
 });
 
-// @route   POST api/auth
-//@desc     Auth user & get token
-//@access   Public
-router.get("/", (req, res) => {
+// @route     POST api/auth
+// @desc      Auth user & get token
+// @access    Public
+router.post("/", (req, res) => {
   res.send("Log in user");
 });
 
--- routes/contacts.js
+++ routes/contacts.js
@@ -1,30 +1,35 @@
 const express = require("express");
+
 const router = express.Router();
 
-// @route   GET api/contacts
-//@desc     Get all users contacts
-//@access   Private
+// @route     GET api/contacts
+// @desc      Get all users contacts
+// @access    Private
+
 router.get("/", (req, res) => {
   res.send("Get all contacts");
 });
 
-// @route   POST api/contacts
-//@desc     Add new contacts
-//@access   Private
+// @route     POST api/contacts
+// @desc      Add new contact
+// @access    Private
+
 router.post("/", (req, res) => {
   res.send("Add contact");
 });
 
-// @route   PUT api/contacts/:id
-//@desc     Update contact
-//@access   Private
+// @route     PUT api/contacts/:id
+// @desc      Update contact
+// @access    Private
+
 router.put("/:id", (req, res) => {
   res.send("Update contact");
 });
 
-// @route   DELETE api/contacts/:id
-//@desc     Delete contact
-//@access   Private
+// @route     DELETE api/contacts/:id
+// @desc      Delete contact
+// @access    Private
+
 router.delete("/:id", (req, res) => {
   res.send("Delete contact");
 });
